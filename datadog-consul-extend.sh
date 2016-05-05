08d72c332f0c7df62a76a7593303193cdff6f3e3
Viet Pham <phamhongviet52@gmail.com>
2016-05-05 21:03:58 +0700

    add consul

--- datadog-consul-extend.sh
+++ datadog-consul-extend.sh
@@ -7,6 +7,15 @@ main() {
 check_env() {
 	test -z "$API_KEY" && exit 1
 	test -z "$API_ENDPOINT" && export API_ENDPOINT='https://app.datadoghq.com/api/'
+	test -z "$CONSUL_API" && export CONSUL_API='http://localhost:8500/v1/'
+}
+
+get_services_list() {
+	curl -s "${CONSUL_API}catalog/services" | jq -r 'keys | .[]'
+}
+
+get_service_hosts_number() {
+	curl -s "${CONSUL_API}health/service/${1}?passing" | jq length
 }
 
 main $@
